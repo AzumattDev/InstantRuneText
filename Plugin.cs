e2d9341539a276d6670a0c97f2c21f0631f9df09
AzumattDev <azumattgaming@gmail.com>
2023-01-19 16:04:47 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace InstantRuneText
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class InstantRuneTextPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "InstantRuneText";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+        }
+    }
+
+    [HarmonyPatch(typeof(TextViewer), nameof(TextViewer.ShowText))]
+    static class TextViewerShowTextPatch
+    {
+        static void Prefix(TextViewer __instance, TextViewer.Style style, string topic, string text, bool autoHide)
+        {
+            if (style != TextViewer.Style.Rune) return;
+            if (Player.m_localPlayer == null) return;
+            
+            // Set this to a high value to make the text appear instantly. 15 seems too low..and I wasn't about to mess with it for 20 minutes
+            // so I said fuck it and made it fast as shit.
+            __instance.m_animator.speed = 500.0f;
+        }
+    }
+}
